0d9a3211e21d661086e35a5d8b0dc9228065c023
Digambar Deshmukh <digambardeshmukh12@gmail.com>
2024-12-01 14:35:47 +0530

    Changes in Email Templete

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -11,71 +11,80 @@ import {
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css'],
   animations: [
-    trigger('slideIn', [
+    trigger('fadeIn', [
       transition(':enter', [
-        style({ opacity: 0, transform: 'translateX(-100%)' }), // Slide in from left
-        animate('0.5s ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
-      ]),
-      transition(':leave', [
-        animate('0.5s ease-in', style({ opacity: 0, transform: 'translateX(-100%)' }))
-      ])
-    ]),
-    trigger('slideOut', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translateX(100%)' }), // Slide in from right
-        animate('0.5s ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
-      ]),
-      transition(':leave', [
-        animate('0.5s ease-in', style({ opacity: 0, transform: 'translateX(100%)' }))
+        style({ opacity: 0 }),
+        animate('600ms', style({ opacity: 1 }))
       ])
     ])
   ]
 })
 export class ContactComponent {
   currentForm: 'contact' | 'hire' = 'contact';
+
   formData = {
     name: '',
     email: '',
     message: '',
+    hireEmail: '',
     projectName: '',
     budget: '',
     details: '',
-    hireEmail: ''
   };
+
+
+
+  constructor(private _snackBar: MatSnackBar) { }
+
+
+
+
+  openSnackBar(message: string, responseType: 'S' | 'E') {
+    const panelClass = responseType === 'S' ? 'snackbar-success' : 'snackbar-error';
+    this._snackBar.open(message, 'Close', {
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      duration: 3000,
+      panelClass: [panelClass]
+    });
+  }
+
+
+  sendEmail() {
+
+    const serviceID = 'service_xbd7vmg'; // Replace with your EmailJS service ID
+    const templateID = 'template_y2wdo1p'; // Replace with your EmailJS template ID
+    const userID = 'DymUODE7IAA0gvXDW'; // Replace with your EmailJS user ID
+
+    const templateParams = {
+      name: this.formData.name,
+      email: 'digambardeshmukhoffice@gmail.com',
+      message: this.formData.message + " " + this.formData.email
+    };
+
+    emailjs.send(serviceID, templateID, templateParams, userID)
+      .then((response: EmailJSResponseStatus) => {
+        this.openSnackBar('Email sent successfully!', 'S');
+        this.formData = {
+          name: '',
+          email: '',
+          message: '',
+          hireEmail: '',
+          projectName: '',
+          budget: '',
+          details: '',
+        };
+      }, (error) => {
+        this.openSnackBar('Failed to send email!', 'E')
+      });
+  }
+
 
   toggleForm(form: 'contact' | 'hire') {
     this.currentForm = form;
   }
-
-  
-
   sendQuote() {
     console.log('Quote requested with data:', this.formData);
   }
 
-  sendEmail() {
-    
-      const serviceID = 'service_xbd7vmg'; // Replace with your EmailJS service ID
-      const templateID = 'template_y2wdo1p'; // Replace with your EmailJS template ID
-      const userID = 'DymUODE7IAA0gvXDW'; // Replace with your EmailJS user ID
-
-      const templateParams = {
-        name: this.formData.name,
-        email: 'digambardeshmukhoffice@gmail.com',
-         message: this.formData.message + " " +  this.formData.email
-      };
-
-      emailjs.send(serviceID, templateID, templateParams, userID)
-        .then((response: EmailJSResponseStatus) => {
-           this.openSnackBar('Email sent successfully!' , 'S');
-           this.formData = {
-            name: '',
-            email: '',
-            message: ''
-          };
-         }, (error) => {
-          this.openSnackBar('Failed to send email!' , 'E')
-          });
-    }
-  
 }
